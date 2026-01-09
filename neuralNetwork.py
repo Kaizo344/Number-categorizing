f88423b45a223f0f82f0a7f3446d2bfc63d2628b
kai_fynn <kaifynn9@gmail.com>
2026-01-09 11:03:07 +0200

    One-hot encoding scheme as well as hidden layer prep and formating

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,27 +1,61 @@
 import numpy as np
 import struct 
 
-##  we start by importing and normalising the data 
-##images
+###  we start by importing and normalising the data 
+### images
 def loadImages(fileName):
     with open(fileName, 'rb') as f:
         magic, num, rows, cols = struct.unpack(">IIII", f.read(16))
-        images = np.frombuffer(f.read(), dytpe=np.unit8)
+        images = np.frombuffer(f.read(), dtype=np.uint8)
         images = images.reshape(num, rows, cols)
 
     return images
 
-##labels
+###labels
 def loadLabels(fileName):
     with open(fileName, 'rb') as l:
         magic, num = struct.unpack('>II', l.read(8))
-        labels = np.frombuffer(l.read(), dtype=np.uinit8)
+        labels = np.frombuffer(l.read(), dtype=np.uint8)
     return labels
 
-## load the data sets
-Xtrain = loadImages("train-images.idx3-ubyte")
-xtrain = loadLabels("train-labels.idx1-ubyte")
+### load the data sets
+XTrain = loadImages("train-images.idx3-ubyte")
+YTrain = loadLabels("train-labels.idx1-ubyte")
 
-Ytrain = loadImages("t10k-images.idx3-ubyte")
-ytrain = loadLabels("t10k-labels.idx1-ubyte")
+XTest = loadImages("t10k-images.idx3-ubyte")
+YTest= loadLabels("t10k-labels.idx1-ubyte")
+
+### data preparation
+
+## normalise the data into a proportion
+XTrain = XTrain.reshape(XTrain.shape[0], -1).astype(np.float32)/255.0
+XTest  = XTest.reshape(XTest.shape[0], -1).astype(np.float32)/255.0
+
+# print("Training data: ")
+# print(XTrain.shape)
+# print("Testing data: ")
+# print(XTest.shape)
+
+
+## one hot encoding 
+def oneHot(y, numclasses = 10):
+    return np.eye(numclasses)[y]
+
+YTrainO = oneHot(YTrain)
+YTestO = oneHot(YTest)
+
+### forward pass
+
+inputSize = 784
+hiddenSize = 128
+outputSize = 10
+
+##input
+W1 = np.random.randn(inputSize , hiddenSize) * 0.01
+B1 = np.zeros((1,hiddenSize))
+
+##output
+W2 = np.random.randn(inputSize, hiddenSize) * 0.01
+B2 = np.random.randn((1,outputSize)) 
+
 
